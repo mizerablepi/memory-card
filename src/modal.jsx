c9c7a4cf86a8b5f9937c56edbb68241bb6d3c3a3
miz <mizerablepi@gmail.com>
2023-08-23 21:40:36 +0530

    change img position

--- src/modal.jsx
+++ src/modal.jsx
@@ -3,7 +3,7 @@ export default function Modal({ clickHandler }) {
     <div className="absolute flex justify-center items-center w-screen h-screen bg-[rgba(0,0,0,0.3)]">
       <div className="p-6 bg-white rounded-lg h-[20rem] w-[30rem] flex flex-col justify-between text-center items-center">
         <h1 className="font-bold text-5xl">Game Over</h1>
-        <img src="./src/sp.jpg" alt="" className="h-40 object-contain" />
+        <img src="./public/sp.jpg" alt="" className="h-40 object-contain" />
         <button
           className="text-2xl font-bold bg-[#ffcc01] border-4 border-[#2e68b3] px-6"
           onClick={clickHandler}
